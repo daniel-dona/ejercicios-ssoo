c56092870f3ac72e73fbc995e8b5d8d1b287431c
agonzalo05 <malagonero.97@gmail.com>
2017-12-31 17:10:57 +0100

    Create Agr.c

---
+++ Agr.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+//Como no se bien c, antes de tocar nada de lo que tienes hecho, prefiero hacer uno a parte Y asi puedo hacer lo que quiera y no jodo nada.
+
+int main () 
+{
+
+printf ("hola mundo");
+
+}
